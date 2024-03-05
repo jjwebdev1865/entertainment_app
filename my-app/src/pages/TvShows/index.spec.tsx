3bfab2d1e78414f875bc410e412396d3314b72a3
2D\james.jiracek <james.jiracek@slalom.com>
2024-03-04 19:42:18 -0500

    creating tv shows page

---
+++ my-app/src/pages/TvShows/index.spec.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import '@testing-library/jest-dom';
+import TvShows from ".";
+
+describe('TvShows', () => {
+
+  it('contains the correct headers', () => {
+    render(<TvShows />);
+    const headingOne = screen.getByRole('heading', {level: 1});
+    expect(headingOne).toBeVisible();
+  })
+})
