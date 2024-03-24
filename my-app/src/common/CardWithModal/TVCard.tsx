236f845f9a6bb8532e20a09d3fc39045d8423092
2D\james.jiracek <james.jiracek@slalom.com>
2024-03-23 22:15:00 -0400

    modal basics part three

--- my-app/src/common/CardWithModal/TVCard.tsx
+++ my-app/src/common/CardWithModal/TVCard.tsx
@@ -3,17 +3,30 @@ import Modal from "react-modal";
 import * as styles from './CardWithModal.styles';
 import { Show } from "../../types/models";
 import { formatSeenString } from "../../api/local_api/format";
+import styled from "styled-components";
 
 type TVCardProps = {
   show: Show;
 }
+
+const StyledModalContent = styled.div`
+  float: right;
+  width: 50%;
+  margin: 0;
+`;
+
+const StyledModalImage = styled.div`
+  float: left;
+  width: 50%;
+  margin: 0;
+`;
 
 export const TVCard = (props: TVCardProps) => {
   const [modalOpen, setModalOpen] = useState(false);
 
   const { modalStyles, StyledPoster } = styles;
   const { show } = props;
-  const { endDate, poster, rating, releaseDate, seasons, seen, title } = show;
+  const { endDate, id, poster, rating, releaseDate, seasons, seen, title } = show;
   const seenString = formatSeenString(seen);
 
   return (
@@ -26,13 +39,20 @@ export const TVCard = (props: TVCardProps) => {
         onRequestClose={() => setModalOpen(false)}
         style={modalStyles}
       >
-        <div><strong>Title:</strong> {title}</div>
-        <div><strong>Seasons:</strong> {seasons}</div>
-        <div><strong>TV Rating:</strong> {rating}</div>
-        <div><strong>Runtime:</strong> {releaseDate} - {endDate}</div>
-        <div><strong>Seen:</strong> {seenString}</div>
-
-        <button onClick={() => setModalOpen(false)}>Close Modal</button>
+        <div data-testid={`tv-show-card-modal-container-${id}`}>
+          <StyledModalImage>
+            <img src={poster} style={{ width: '100%', height: '100%'}} />
+          </StyledModalImage>
+          <StyledModalContent data-testid={`tv-show-card-modal-content-${id}`}>
+            <h3><strong>Title:</strong> {title} </h3>
+            
+            <div><strong>Seasons:</strong> {seasons}</div>
+            <div><strong>TV Rating:</strong> {rating}</div>
+            <div><strong>Runtime:</strong> {releaseDate} - {endDate}</div>
+            <div><strong>Seen:</strong> {seenString}</div>
+            <button onClick={() => setModalOpen(false)}>Close Modal</button>
+          </StyledModalContent>
+        </div>
       </Modal>
     </li>
   );
