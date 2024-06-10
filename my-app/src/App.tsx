10ed8a3bcafc1e8cbb9f9d527fc384a325e2423c
2D\james.jiracek <james.jiracek@slalom.com>
2024-06-10 09:28:31 -0400

    initial commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './pages/router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
--- my-app/src/pages/Home/index.tsx
+++ my-app/src/pages/Home/index.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import { getFavorites } from '../../api/local_api/filter';
 import { movies } from '../../config/movies';
 import { Navbar } from '../../components';
-import { FavoritesTile } from '../../common';
-import styled from 'styled-components';
-
-const StyledFavorites = styled.ul`
-  list-style: none;
-`;
+import { Carousel } from 'react-bootstrap';
 
 const Home = (): JSX.Element => {
   const favorites = getFavorites(movies);
@@ -19,17 +14,20 @@ const Home = (): JSX.Element => {
 
       <div data-testid="favorites-container">
         <h2 data-testid="favorites-header">Favorites</h2>
-        <StyledFavorites>
-          {favorites.map((item) => {
-            return (
-              <FavoritesTile
-                key={`favorite-movie-${item.id}`}
-                title={item.title}
-                id={item.id}
-              />
-            );
-          })}
-        </StyledFavorites>
+        <div style={{ border: '2px solid black', width: '100%'}}>
+          <Carousel controls={false}>
+            {favorites.map((item) => {
+              return (
+                <Carousel.Item key={`favorite-movie-${item.id}`} interval={5000}>
+                  <img src={item.poster} height={500} width={400} />
+                  <Carousel.Caption as={'div'}>
+                    <h3>{item.title}</h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </div>
       </div>
     </div>
   );
